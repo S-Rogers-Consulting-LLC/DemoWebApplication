2f143a4c1690f96063809ba7c99f410f62240104
Stewart.Rogers <StewartRogers86@HotMail.com>
2015-11-11 17:20:09 -0700

    Implemented my first Knockout Component.

--- Console/Modules/Bootstrapper.cs
+++ Console/Modules/Bootstrapper.cs
@@ -1,6 +1,11 @@
-﻿using log4net;
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using log4net;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.Conventions;
+using Nancy.TinyIoc;
 
 namespace WebSite.Modules {
     public class Bootstrapper : DefaultNancyBootstrapper {
@@ -9,9 +14,49 @@ namespace WebSite.Modules {
         #endregion
 
         protected override void ConfigureConventions(NancyConventions argNancyConventions) {
-            base.ConfigureConventions(argNancyConventions);
             argNancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory(@"/Scripts"));
             argNancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory(@"/Images"));
+            argNancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory(@"/Fonts"));
+
+            base.ConfigureConventions(argNancyConventions);
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer argTinyIoCContainer, IPipelines argPipelines) {
+            argPipelines.AfterRequest += ModifyModel;
+            base.ApplicationStartup(argTinyIoCContainer, argPipelines);
+        }
+
+        private void ModifyModel(NancyContext argNancyContext) {
+            CheckForIfNonMatch(argNancyContext);
+            CheckForIfModifiedSince(argNancyContext);
+        }
+
+        private void CheckForIfNonMatch(NancyContext argNancyContext) {
+            var request = argNancyContext.Request;
+            var response = argNancyContext.Response;
+
+            var responseETag = string.Empty;
+            if (!response.Headers.TryGetValue("ETag", out responseETag))
+                return;
+
+            if (request.Headers.IfNoneMatch.Contains(responseETag))
+                argNancyContext.Response = HttpStatusCode.NotModified;
+        }
+
+        private void CheckForIfModifiedSince(NancyContext argNancyContext) {
+            var request = argNancyContext.Request;
+            var response = argNancyContext.Response;
+
+            string responseLastModified;
+            if (!response.Headers.TryGetValue("Last-Modified", out responseLastModified))
+                return;
+
+            DateTime lastModified;
+            if (!request.Headers.IfModifiedSince.HasValue || !DateTime.TryParseExact(responseLastModified, "R", CultureInfo.InvariantCulture, DateTimeStyles.None, out lastModified))
+                return;
+
+            if (lastModified <= request.Headers.IfModifiedSince.Value)
+                argNancyContext.Response = HttpStatusCode.NotModified;
         }
     }
 }
--- Console/Scripts/Main.js
+++ Console/Scripts/Main.js
@@ -1,16 +1,33 @@
 ﻿function loadCss(url) { var link = document.createElement("link"); link.type = "text/css"; link.rel = "stylesheet"; link.href = url; document.getElementsByTagName("head")[0].appendChild(link); }
-function loadIcon(url) { var link = document.createElement("link"); link.rel = "icon"; link.href = url; document.getElementsByTagName("head")[0].appendChild(link); }
+function loadIcon(url) { var iconLink = document.createElement("link"); iconLink.rel = "icon"; iconLink.href = url; document.getElementsByTagName("head")[0].appendChild(iconLink); }
 
-loadIcon('/favicon.ico');
-loadCss('Content/bootstrap.css');
-loadCss('Content/bootstrap-theme.css');
+loadIcon("Images/favicon.ico");
+loadCss("Content/bootstrap.css");
+loadCss("Content/bootstrap-theme.css");
 
-requirejs(["Scripts/jquery-2.1.4.js", "Scripts/bootstrap.js", "Scripts/knockout-3.3.0.js"], function (jquery, bootstrap, ko) {
+require(["Scripts/jquery-2.1.4.js", "Scripts/bootstrap.js", "Scripts/knockout-3.3.0.js"], function ($, b, ko) {
+
+    ko.components.register("textbox-component", {
+        viewModel: function (params) {
+            var self = this;
+            self.caption = ko.observable(params.initialCaption);
+            self.placeholder = ko.observable(params.initialPlaceholder);
+            self.value = ko.observable(params.initialValue);
+        },
+        template: '<div class="input-group input-group-sm">'
+                    + '<span class="input-group-addon" data-bind="text: caption"></span>'
+                    + '<input type="text" class="form-control" placeholder="" aria-describedby="sizing-addon3" data-bind="attr: {placeholder: placeholder}, value: value">'
+                  + '</div>'
+    });
+
     var ViewModel = function (first, last) {
-        this.firstName = ko.observable(first);
-        this.lastName = ko.observable(last);
-        this.fullName = ko.pureComputed(function () { return this.firstName() + " " + this.lastName(); }, this);
+        var self = this;
+        self.firstName = ko.observable(first);
+        self.lastName = ko.observable(last);
+        self.fullName = ko.pureComputed(function () { return self.firstName() + " " + self.lastName(); }, this);
     };
 
     ko.applyBindings(new ViewModel("Stewart", "Rogers"));
-});
+})
+
+
